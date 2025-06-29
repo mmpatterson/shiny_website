a0236f1576977f6f96608b1a66bcc2210f9dbdbb
Max Patterson <mmpatterson94@gmail.com>
2025-06-29 00:31:38 -0400

    Slowly adjusting the theme and adding in more to the scrollychat

--- app.R
+++ app.R
@@ -16,40 +16,52 @@ library(bsicons)
 library(scrollytell)
 library(leaflet)
 library(htmltools)
+library(nflfastR)
+library(dplyr)
+library(gt)
+library(gtExtras)
+library(DT)
+library(ggplot2)
+library(ggimage)
+library(glue)
 
 # Define UI for the application
 ui <- fluidPage(
     
-  theme = shinytheme("flatly"),
+  # theme = bs_theme(brand = TRUE),
   # shinythemes::themeSelector(),
-  # tags$head(
-  #   includeCSS("./www/styles.css")
-  # ),
   tags$head(
-    tags$style(HTML("
-      #fadeContainer {
-        transition: opacity 2.5s ease-in-out;
-        opacity: 1;
-      }
-    ")),
+    includeCSS("./www/styles.css")
+  ),
+  tags$head(
+    # tags$style(HTML("
+    #   #fadeContainer {
+    #     opacity: 1;
+    #     transition: opacity 0.5s ease-in;
+    #   }
+    # ")),
     tags$script(HTML("
       Shiny.addCustomMessageHandler('fade', function(message) {
         const container = document.getElementById('fadeContainer');
         if (!container) return;
+      
+        // Temporarily disable transition to instantly hide
+        container.style.transition = 'none';
         container.style.opacity = 0;
-        setTimeout(function() {
-          container.style.opacity = 1;
-        }, 100);
+      
+        // Force a reflow to apply the style immediately
+        void container.offsetWidth; // trick to flush styles
+      
+        // Re-enable transition and fade in
+        container.style.transition = 'opacity 0.5s ease-in';
+        container.style.opacity = 1;
       });
     "))
   ),
   # Create a top bar with dropdowns
-  navbarPage(
+  page_navbar(
     title = "Maxwell Patterson",
-    tabPanel("",icon = icon("home", lib = "glyphicon"),
-             h1("Welcome to My Personal Website"),
-             p("This is the home page where you can find an overview of my work and links to other sections of the site."),
-             p("Use the navigation bar above to access my resume, LinkedIn profile, and projects.")
+    tabPanel("",icon = icon("home", lib = "glyphicon")
     ),
     tabPanel(title = tags$div(
       onclick = "https://www.linkedin.com/in/maxwell-patterson/",
@@ -100,29 +112,38 @@ ui <- fluidPage(
                # Add more projects as needed
     )
   ),
-  sidebarLayout( sidebarPanel("About me"),
-       tabsetPanel(id="tab",
-           tabPanel("bla",
-                fluidRow(
-                  scrolly_container("scr", 
-                      scrolly_graph(
-                        div(id = "fadeContainer", uiOutput("dynamicOutput"))
-                      ), 
-                      scrolly_sections(
-                        scrolly_section(id = "red",
-                          h3("About me"),
-                          p("Hello! My name is Max Patterson, and I am a data scientist in the Greater Boston area")
-                        ),
-                        scrolly_section(id = "pink","I have a passion for data and crafting insightful stories with it"),
-                        scrolly_section(id = "locations","I've been lucky to live in a few different cities in my life"),
-                        scrolly_section(id = "bills","I was born and raised in Buffalo, NY. I am a diehard Buffalo Bills fan. I also like the Buffalo Sabres, but it's hard rooting for such a downtrodden team.")
+
+  scrolly_container("scr",
+                    scrolly_graph(
+                      div(id = "fadeContainer", uiOutput("dynamicOutput"))
+                    ),
+                    
+                    scrolly_sections(id = "scrolly-section",
+                                     
+                      scrolly_section(id = "intro",
+                                      
+                                      p("Hello! My name is Max Patterson, and I am a data scientist in the Greater Boston area")
+                      ),
+                      scrolly_section(id = "data",
+                                      "I have a passion for data and crafting insightful stories with it"
+                      ),
+                      scrolly_section(id = "locations",
+                                      "I've been able to call 5 different cities my home"
+                      ),
+                      scrolly_section(id = "bills",
+                                      "I was born and raised in Buffalo, NY. I am a diehard Buffalo Bills fan. I also like the Buffalo Sabres, but it's hard rooting for such a downtrodden team"
+                      ),
+                      scrolly_section(id = "education",
+                                      "I attended Dickinson College in Carlisle, PA, where I earned my bachelor's in physics and mathematics and participated on the cross country and track & field teams. I also have a master's in data science from the University of Texas at Austin"
+                      ),
+                      scrolly_section(id = "career-high-level",
+                                      "I started out of undergrad as an IT consultant, and I kept getting drawn to the technical portions of my work. I decided to do a data science bootcamp, and I realized that a career in data science was what I was looking for."
+                      ),
+                      scrolly_section(id = "data-science-specifics",
+                                      glue::glue("I have ", as.numeric(format(Sys.Date(), "%Y")) - 2016, " years of experience in SQL and ", as.numeric(format(Sys.Date(), "%Y")) - 2019, " years of experience in python and R" )
                       )
                   )
-               )
-          )
-       )
-  ),
-  div("Footer")
+  )
 )
 
 
@@ -131,18 +152,105 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   addResourcePath("data", "./data")
   
+  # Perform NFL Data Calculations
+  pbp <- calculate_stats(
+    seasons = nflreadr::most_recent_season(),
+    summary_level = c("season"),
+    stat_type = c("team"),
+    season_type = c("REG")
+  ) %>%
+    select(team, passing_yards, passing_yards_after_catch) %>%
+    mutate(passing_yards_before_catch = passing_yards - passing_yards_after_catch) %>%
+    inner_join(
+      teams_colors_logos %>%
+        select(team_abbr, team_wordmark, team_logo_espn),
+      by = c('team' = 'team_abbr')
+    )
+  
+  pbp_data <- renderDT(as.data.frame(pbp %>% head(10)))
+  
+  team_table <- gt(pbp %>% select(team_wordmark, team_logo_espn, passing_yards, passing_yards_before_catch, passing_yards_after_catch)) %>%
+    cols_move(
+      columns = passing_yards_after_catch,
+      after = passing_yards_before_catch
+    ) %>%
+    gt_img_rows(columns = team_wordmark, height = 25) %>%
+    gt_img_rows(columns = team_logo_espn, img_source = "web", height = 30) %>%
+    cols_label(
+      passing_yards = "Passing Yards",
+      passing_yards_before_catch = "Passing Yards Before Catch",
+      passing_yards_after_catch = "Passing Yards After Catch",
+      team_wordmark = "",
+      team_logo_espn = ""
+    ) 
+  
+  med_before_catch <- median(pbp$passing_yards_before_catch)
+  med_after_catch <- median(pbp$passing_yards_after_catch)
+  
+  team_plot <- renderPlot(
+    pbp %>%
+    ggplot(aes(x = passing_yards_before_catch, y = passing_yards_after_catch)) + 
+    geom_image(aes(image = team_logo_espn)) +
+    geom_vline(xintercept = med_before_catch) +
+    geom_hline(yintercept = med_after_catch) +
+    labs(
+      title = "Passing Yards Before/After Catch"
+    ) +
+    xlab("Passing Yards Before Catch") +
+    ylab("Passing Yards After Catch")
+  )
+  
+  timer <- reactiveTimer(3000)
+  options <- c("pbp_data", "team_table", "team_plot")
+  print(options)
+  counter <- reactiveVal(1)
+  
+  observe({
+    timer()
+    
+    isolate({
+    new_val <- counter() %% length(options) + 1
+    counter(new_val)
+    })
+  
+  })
+  
   output$dynamicOutput <- renderUI({
-    if(input$scr == "locations"){
+    if(input$scr == "data"){
+      val <- options[counter()]
+      session$sendCustomMessage("fade", list())
+      if(val == "pbp_data"){
+        pbp_data
+      }
+      else if(val == "team_table") {
+        render_gt(team_table)
+      }
+      else if(val == "team_plot"){
+        tags$div(id = "shiny-plot-output", team_plot)
+      }
+        
+    }
+    else if(input$scr == "locations"){
       leafletOutput("cityMap", height = "60vh", width = "60vw")
     }
-    
+
     else if (input$scr == "bills"){
       imageOutput("bills")
     }
     else{
-      imageOutput("bills")
+      
     }
   })
+  
+  # output$dynamicOutput <- renderUI({
+  #   req(input$scr)
+  #   switch(input$scr,
+  #          "intro" = h2("Intro Panel: Overview of Max"),
+  #          "pink" = h2("Pink Panel: Data Passion"),
+  #          "locations" = h2("Locations Panel: Where I've Lived"),
+  #          "bills" = h2("Bills Panel: Sports Fandom")
+  #   )
+  # })
   
   
   # Locations Map
@@ -165,8 +273,8 @@ server <- function(input, output, session) {
     list(
       src = './www/Bills_Photo.jpeg',
       contentType = 'image/jpeg',
-      width = "50%",
-      heiht = "30%",
+      width = "60%",
+      height = "70%",
       alt = "Me at a Bills game"
     )
   }, deleteFile = FALSE)
